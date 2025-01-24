18f679635b57caca38c5753513db318d398529e4
mandar <sakpalm466@gmail.com>
2025-01-24 07:30:27 +0530

    added db and seed scripts

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client"
+
+const prismaSingleton = () => (new PrismaClient())
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaSingleton>
+}
+
+const prisma = global.prisma || prismaSingleton()
+
+export default prisma
+
+
+// in production mode as the application is build only once this file will be imported and parsed only once 
+// so it wuill directly take the prismaSingleton instance at that time 
+// where as in development mode as the application is build everytime we make changes to the code 
+// as in development the global instance is only created once and not on every hot load 
+//we can use the global instance to keep the prisma instance alive between hot reloads
+if(process.env.NODE_ENV !== "production") {
+  global.prisma = prisma
+}
