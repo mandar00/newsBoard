5465ab6306529f3d5cfe614d2dc780bad59d4382
mandar <sakpalm466@gmail.com>
2025-01-25 18:56:46 +0530

    implemented bottom nav bar

--- src/components/serverComponents/Navbar/FooterNav.tsx
+++ src/components/serverComponents/Navbar/FooterNav.tsx
@@ -1,12 +1,16 @@
 import { navbarLinks } from "@/app/news/constants";
+import Link from "next/link";
 
 export default function FooterNav(){
   return(
-    <div className="flex items-center justify-evenly md:hidden z-10 fixed w-full bottom-0 h-[50px] bg-lime-300 py-2 px-5">
+    <div className="flex items-center justify-evenly md:hidden z-10 fixed w-full bottom-0 h-[60px] bg-white py-2 px-5 shadow-md">
       {
         navbarLinks.map((link,linkIndex)=>{
           return(
-            <></>
+            <Link href={link.link} key={linkIndex} className="flex flex-col items-center justify-center sm:text-[12px] text-[10px]">
+              <link.icon  className="sm:w-[18px] w-[15px]"/>
+              {link.title}
+            </Link>
           )
         })
       }
